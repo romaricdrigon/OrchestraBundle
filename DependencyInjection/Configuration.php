39dec2ca4aea90c4c2fcc633c50e865db5b5d100
Romaric Drigon <romaric.drigon@gmail.com>
2014-07-18 13:33:43 +0200

    Added bundle configuration files

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace RomaricDrigon\OrchestraBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('orchestra_bundle');
+
+        // define parameters
+
+        return $treeBuilder;
+    }
+}
---
+++ RomaricDrigonOrchestraBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RomaricDrigon\OrchestraBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RomaricDrigonOrchestraBundle extends Bundle
+{
+}
