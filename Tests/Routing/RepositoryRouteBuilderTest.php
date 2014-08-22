aae26916a900f1e0361201a2ceb8e744c5fc17a0
Romaric Drigon <romaric.drigon@gmail.com>
2014-08-22 17:57:01 +0200

    Added RepositoryRouteBuilder test

---
+++ Tests/Routing/RepositoryRouteBuilderTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Orchestra project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RomaricDrigon\OrchestraBundle\Tests\Routing;
+
+use RomaricDrigon\OrchestraBundle\Routing\RepositoryRouteBuilder;
+
+/**
+ * Class RepositoryRouteBuilderTest
+ * @author Romaric Drigon <romaric.drigon@gmail.com>
+ */
+class RepositoryRouteBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var RepositoryRouteBuilder
+     */
+    private $sut;
+
+    public function setUp()
+    {
+        $pool = \Phake::mock('RomaricDrigon\OrchestraBundle\Pool\RepositoryPool');
+
+        \Phake::when($pool)->all()->thenReturn([
+            'mock1' => 'anything',
+            'mock2' => 'not used'
+        ]);
+
+        $this->sut = new RepositoryRouteBuilder($pool);
+    }
+
+    public function test_it_builds_route_collection()
+    {
+        $this->assertNotNull($routes = $this->sut->getCollection());
+
+        $this->assertEquals(2, count($routes));
+
+        $this->assertNotNull($route1 = $routes->get('orchestra_repository_mock1_list'));
+
+        $this->assertEquals('/mock1/list', $route1->getPath());
+
+        $this->assertNotNull($route2 = $routes->get('orchestra_repository_mock2_list'));
+
+        $this->assertEquals('/mock2/list', $route2->getPath());
+
+        $this->assertNull($routes->get('orchestra_repository_mock3_list'));
+    }
+}
+ 
