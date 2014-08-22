7a8c6dce31ce19b32217b6d6389a224580cb2723
Romaric Drigon <romaric.drigon@gmail.com>
2014-08-22 14:27:13 +0200

    Working basic RepositoryPool

--- Pool/RepositoryPool.php
+++ Pool/RepositoryPool.php
@@ -19,7 +19,37 @@ use RomaricDrigon\OrchestraBundle\Domain\RepositoryInterface;
  */
 class RepositoryPool
 {
+    protected $repositoriesBySlug = [];
+
+    /**
+     * Add a repository to the pool
+     *
+     * @param RepositoryInterface $repository
+     * @throws \Exception if we try to add twice a repository
+     */
     public function addRepository(RepositoryInterface $repository)
     {
+        // At the moment slug is not configurable otherwise
+        $slug = strtolower(get_class($repository));
+
+        if (isset($this->repositoriesBySlug[$slug])) {
+            throw new \Exception('Repository with slug '.$slug.' seems to be registered twice!');
+        }
+
+        $this->repositoriesBySlug[$slug] = $repository;
+    }
+
+    /**
+     * @param string $slug
+     * @return RepositoryInterface
+     * @throws \Exception
+     */
+    public function getBySlug($slug)
+    {
+        if (! isset($this->repositoriesBySlug[$slug])) {
+            throw new \Exception('Unable to find repository for slug '.$slug.'. Maybe you forget to register it with the the orchestra.repository tag?');
+        }
+
+        return $this->repositoriesBySlug[$slug];
     }
 }
