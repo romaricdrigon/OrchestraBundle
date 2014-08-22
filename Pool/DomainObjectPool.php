97de3c01ba29a58abb62b81712e2810bb450bd56
Romaric Drigon <romaric.drigon@gmail.com>
2014-08-22 13:00:22 +0200

    Added pools to represents all Domain objects & repositories

---
+++ Pool/DomainObjectPool.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RomaricDrigon\OrchestraBundle\Pool;
+
+/**
+ * Class DomainObjectPool
+ * @package RomaricDrigon\OrchestraBundle\Pool
+ *
+ * Represents a collection of all Domain Objects usable by Orchestra
+ */
+class DomainObjectPool
+{
+
+}
---
+++ Pool/RepositoryPool.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RomaricDrigon\OrchestraBundle\Pool;
+
+/**
+ * Class RepositoryPool
+ * @package RomaricDrigon\OrchestraBundle\Pool
+ *
+ * Represents a collection of all Repositories usable by Orchestra
+ */
+class RepositoryPool
+{
+
+}
