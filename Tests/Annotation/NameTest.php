4483c6196db2962b650b55a21fdc113604fa22f2
Romaric Drigon <romaric.drigon@gmail.com>
2014-08-25 11:21:42 +0200

    Added test for Name annotation

---
+++ Tests/Annotation/NameTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Orchestra project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RomaricDrigon\OrchestraBundle\Tests\Annotation;
+
+use RomaricDrigon\OrchestraBundle\Annotation\Name;
+
+/**
+ * Class NameTest
+ * @author Romaric Drigon <romaric.drigon@gmail.com>
+ */
+class NameTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_it_accepts_value()
+    {
+        $name = new Name(['value' => 'some value']);
+
+        $this->assertEquals('some value', $name->getName());
+    }
+
+    public function test_it_accepts_name_option()
+    {
+        $name = new Name(['name' => 'some value']);
+
+        $this->assertEquals("some value", $name->getName());
+    }
+
+    public function test_it_does_not_allow_other_options()
+    {
+        $this->setExpectedException('RomaricDrigon\OrchestraBundle\Exception\AnnotationInvalidOption');
+
+        new Name(['value' => 'some value', 'foo' => 'bar']);
+    }
+}
+ 
