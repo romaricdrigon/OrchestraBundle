e3dc022e7b767ed725cdb0d8410d25a546b8765a
Romaric Drigon <romaric.drigon@gmail.com>
2014-09-04 17:57:57 +0200

    Added EventCollection

---
+++ Core/Event/EventCollection.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the Orchestra project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RomaricDrigon\OrchestraBundle\Core\Event;
+
+use RomaricDrigon\OrchestraBundle\Domain\Event\EventCollectionInterface;
+use RomaricDrigon\OrchestraBundle\Domain\Event\EventInterface;
+
+/**
+ * Class EventCollection
+ * @author Romaric Drigon <romaric.drigon@gmail.com>
+ */
+class EventCollection implements EventCollectionInterface
+{
+    /**
+     * @var EventInterface[]
+     */
+    protected $events;
+
+
+    public function __construct()
+    {
+        $this->events = [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function add(EventInterface $event)
+    {
+        $this->events[] = $event;
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function pop()
+    {
+        return array_pop($this->events);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function shift()
+    {
+        return array_shift($this->events);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function count()
+    {
+        return count($this->events);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->events);
+    }
+} 
---
+++ Domain/Event/EventCollectionInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Orchestra project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RomaricDrigon\OrchestraBundle\Domain\Event;
+
+/**
+ * Interface EventCollectionInterface
+ * @author Romaric Drigon <romaric.drigon@gmail.com>
+ */
+interface EventCollectionInterface extends \Countable, \IteratorAggregate
+{
+    /**
+     * @param EventInterface $event
+     * @return $this
+     */
+    public function add(EventInterface $event);
+
+    /**
+     * Get last Event and remove it from the array
+     *
+     * @return EventInterface
+     */
+    public function pop();
+
+    /**
+     * Get first Event and remove it from the array
+     *
+     * @return EventInterface
+     */
+    public function shift();
+} 
