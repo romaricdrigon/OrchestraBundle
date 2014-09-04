ebce5427175b902db7d81c7206a2fcb39a305a2b
Romaric Drigon <romaric.drigon@gmail.com>
2014-09-04 15:12:33 +0200

    Added EntityListener

---
+++ EventListener/EntityListener.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the Orchestra project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RomaricDrigon\OrchestraBundle\EventListener;
+
+use RomaricDrigon\OrchestraBundle\Resolver\EntityRepository\EntityRepositoryResolverInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Class EntityListener
+ * @author Romaric Drigon <romaric.drigon@gmail.com>
+ */
+class EntityListener implements EventSubscriberInterface
+{
+    /**
+     * @var EntityRepositoryResolverInterface
+     */
+    protected $entityRepositoryResolver;
+
+    /**
+     * Our subscriber priority
+     */
+    const PRIORITY = 850;
+
+
+    /**
+     * @inheritdoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [KernelEvents::CONTROLLER => ['onKernelController', self::PRIORITY]];
+    }
+
+    /**
+     * @param EntityRepositoryResolverInterface $entityRepositoryResolver
+     */
+    public function __construct(EntityRepositoryResolverInterface $entityRepositoryResolver)
+    {
+        $this->entityRepositoryResolver = $entityRepositoryResolver;
+    }
+
+    /**
+     * @param FilterControllerEvent $event
+     */
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        $request = $event->getRequest();
+
+        if (! $request->attributes->has('entity')) {
+            return;
+        }
+
+        $entity = $request->attributes->get('entity');
+
+        $repository = $this->entityRepositoryResolver->findForEntity($entity);
+
+        $request->attributes->set('repository', $repository);
+    }
+} 
