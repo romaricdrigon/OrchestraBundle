585f9367327d26902d18f1419744ed11070a89be
Romaric Drigon <romaric.drigon@gmail.com>
2014-09-04 18:02:02 +0200

    Repositories can receive events

---
+++ Domain/Repository/ReceiveEventInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Orchestra project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RomaricDrigon\OrchestraBundle\Domain\Repository;
+
+use RomaricDrigon\OrchestraBundle\Domain\Event\EventInterface;
+
+/**
+ * Interface ReceiveEventsInterface
+ * @author Romaric Drigon <romaric.drigon@gmail.com>
+ */
+interface ReceiveEventInterface
+{
+    public function receive(EventInterface $event);
+} 
