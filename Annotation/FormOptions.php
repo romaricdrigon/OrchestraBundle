cc9da47008478078483637e65449877d06789414
Romaric Drigon <romaric.drigon@gmail.com>
2014-09-04 20:22:42 +0200

    Added FormOptions annotation

---
+++ Annotation/FormOptions.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Orchestra project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RomaricDrigon\OrchestraBundle\Annotation;
+
+/**
+ * Class FormOptions
+ * @author Romaric Drigon <romaric.drigon@gmail.com>
+ *
+ * @Annotation
+ */
+class FormOptions extends AbstractAnnotation
+{
+    /**
+     * @var string
+     */
+    protected $options;
+
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param array $options
+     */
+    public function setOptions($options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getValueProperty()
+    {
+        return 'options';
+    }
+} 
